989cd19592b8552c2dbd417198ad73cf37add1f1
Laya Yarramsetti <layayarramsetti09@gmail.com>
2024-05-17 16:40:29 -0400

    WIP

---
+++ travel_recommendation.js
@@ -0,0 +1,36 @@
+
+        function LoadHomeContent() {
+            document.getElementById('the-content').innerHTML = `
+                <div class="headline">EXPLORE DREAM DESTINATION</div>
+                <div class="social-media-links">
+                    <a href="#">Facebook</a>
+                    <a href="#">Twitter</a>
+                    <a href="#">Instagram</a>
+                </div>
+                <div class="description">Your adventure starts here. Explore new destinations and immerse yourself in diverse cultures. Book your next trip with us today!</div>
+            `;
+        }
+
+        function LoadAboutContent() {
+            document.getElementById('the-content').innerHTML = `
+                <div class="headline">ABOUT US</div>
+                <div class="social-media-links">
+                    <a href="#">Facebook</a>
+                    <a href="#">Twitter</a>
+                    <a href="#">Instagram</a>
+                </div>
+                <div class="description">Your adventure starts here. Explore new destinations and immerse yourself in diverse cultures. Book your next trip with us today!</div>
+            `;
+        }
+
+        function LoadContactContent() {
+            document.getElementById('the-content').innerHTML = `
+                <div class="headline">CONTACT US</div>
+                <div class="social-media-links">
+                    <a href="#">Facebook</a>
+                    <a href="#">Twitter</a>
+                    <a href="#">Instagram</a>
+                </div>
+                <div class="description">Your adventure starts here. Explore new destinations and immerse yourself in diverse cultures. Book your next trip with us today!</div>
+            `;
+        }
